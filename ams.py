1c006e76908f4e6244dd10714ae9ef1ec8fb2733
gks2001 <64645167+gks2001@users.noreply.github.com>
2020-10-23 11:47:31 +0530

    v0.2

--- ams.py
+++ ams.py
@@ -3,62 +3,77 @@ class User:
         self.username = username
         self.usertype = usertype
 
-    def printuserdetails(self):
+    def printdetails(self):
         print("\nUsername: " + self.username + "\nUsertype: " + self.usertype)
 
 class Admin(User):
-    def __init__(self, username, usertype = "admin"):
+    def __init__(self, username):
         self.adminname = username
         self.usertype = "admin"
         self.privilege = 3
     
-    def printadmindetails(self):
+    def printdetails(self):
         print("\nAdmin Name: " + self.adminname + "\nUsertype: " + self.usertype)
 
 class Vendor(User):
-    def __init__(self, username, usertype = "vendor", id = -1):
+    def __init__(self, username, ven_id = -1):
         self.vendorname = username
-        self.vendorid = id
-        self.usertype = usertype
+        self.vendorid = ven_id
+        self.usertype = "vendor"
         self.privilege = 1
 
-    def printvendordetails(self):
+    def printdetails(self):
         print("\nVendor Name: " + self.vendorname + "\nVendor ID: " + self.vendorid + "\nUsertype: " + self.usertype)
 
 class Employee(User):
-    def __init__(self, username, usertype = "employee", id = -1):
+    def __init__(self, username, emp_id = -1):
         self.employeename = username
-        self.employeeid = id
-        self.usertype = usertype
+        self.employeeid = emp_id
+        self.usertype = "employee"
         self.privilege = 1
 
-    def printemployeedetails(self):
+    def printdetails(self):
         print("\nEmployee Name: " + self.employeename + "\nEmployee ID: " + self.employeeid + "\nUsertype: " + self.usertype)
 
 class Owner(User):
-    def __init__(self, username, usertype = "owner", aptno = -1):
+    def __init__(self, username, apt = -1):
         self.ownername = username
-        self.aptno = aptno
-        self.usertype = usertype
+        self.aptno = apt
+        self.usertype = "owner"
         self.privilege = 2
 
-    def printownerdetails(self):
+    def printdetails(self):
         print("\nOwner Name: " + self.ownername + "\nApartment Number: " + self.aptno + "\nUsertype: " + self.usertype)
 
 class Resident(User):
-    def __init__(self, username, usertype = "resident", apt = -1):
+    def __init__(self, username, apt = -1):
         self.residentname = username
-        self.aptno = aptno
-        self.usertype = usertype
+        self.aptno = apt
+        self.usertype = "resident"
         self.privilege = 2
 
-    def printvendordetails(self):
-        print("\nResident Name: " + self.residentname + "\nApartment Number: " + self.aptno + "\nUsertype: " + self.usertype)
+    def printdetails(self):
+        print("\nResident Name: " + self.residentname + "\nApartment Number: " + str(self.aptno) + "\nUsertype: " + self.usertype)
 
 n = int(input("Enter number of users: "))
+userlist = []
 for x in range(0,n):
     name = input("Enter user name: ")
     type1 = input("Enter your user type: ")
-    
-u1 = Admin(name, type1)
-u1.printuserdetails()
+    if type1 == "admin":
+        userlist.append(Admin(name))
+    if type1 == "resident":
+        apt = int(input("Enter apartment number: "))
+        userlist.append(Resident(name,apt))
+    if type1 == "owner":
+        apt = input("Enter apartment number: ")
+        userlist.append(Owner(name,apt))
+    if type1 == "vendor":
+        vid = input("Enter vendor id: ")
+        userlist.append(Vendor(name,vid))
+    if type1 == "employee":
+        eid = input("Enter employee id: ")
+        userlist.append(Employee(name,eid))
+
+for x in userlist:
+    x.printdetails()
